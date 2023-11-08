f3008ba0d8f60021867c86e8a4459474d03691d2
wangkai-tech23 <wangkai.2017@tsinghua.org.cn>
2023-11-08 16:46:46 +0800

    all files for LiPar (without weight results)

---
+++ DWParNet/par_DWconv3.py
@@ -0,0 +1,99 @@
+from torch import nn
+import torch
+
+# def _make_divisible(ch, divisor=8, min_ch=None):  # 将通道数调整为divisor的整倍数，min-ch是最小通道数设置
+#     """
+#     This function is taken from the original tf repo.
+#     It ensures that all layers have a channel number that is divisible by 8
+#     It can be seen here:
+#     https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+#     """
+#     if min_ch is None:
+#         min_ch = divisor
+#     new_ch = max(min_ch, int(ch + divisor / 2) // divisor * divisor)
+#     # int后面这一段的目的是将ch调整到最近的divisor整倍数的数值；
+#     # 类似四舍五入的操作，（ch+divisor/2）/divisor取整，然后再乘以divisor则可以得到整倍数的数值；
+#     # "//"表示将结果自动向下取整；
+#     # 整体意思：余数超过4则向上取整，否则向下取整
+#
+#     # Make sure that round down does not go down by more than 10%. 确保向下取整时，不会比ch少10%
+#     if new_ch < 0.9 * ch:  # 若小于则再加一倍
+#         new_ch += divisor
+#     return new_ch
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_channel, out_channel, kernel_size=3, stride=1, groups=1):
+        # group如果等于输入特征模型的深度，则为DW卷积
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU6(inplace=True)
+        )
+
+class ParDW(nn.Module):
+    def __init__(self, num_classes=1000, init_weights=False):
+        super(ParDW, self).__init__()
+
+        # self.branch1 = nn.Sequential(
+        #     ConvBNReLU(3, 64, kernel_size=1),
+        #     ConvBNReLU(64, 64, stride=8, groups=64)
+        # )
+
+        self.branch2 = nn.Sequential(
+            ConvBNReLU(3, 128, kernel_size=1),
+            ConvBNReLU(128, 128, stride=4, groups=128),
+            ConvBN(128, 256, kernel_size=1),
+            ConvBNReLU(256, 256, stride=2, groups=256)
+        )
+
+        self.branch3 = nn.Sequential(
+            ConvBNReLU(3, 32, kernel_size=1),
+            ConvBNReLU(32, 32, stride=2, groups=32),
+            ConvBN(32, 96, kernel_size=1),
+            ConvBNReLU(96, 96, stride=2, groups=96),
+            ConvBN(96, 192, kernel_size=1),
+            ConvBNReLU(192, 192, stride=2, groups=192)
+        )
+
+        self.conv = ConvBN(448, 448)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(448, num_classes)
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        # branch1 = self.branch1(x)
+        branch2 = self.branch2(x)
+        branch3 = self.branch3(x)
+
+        outputs = [branch2, branch3]
+        x = torch.cat(outputs, 1)  # 在channel纬度上进行合并，1是channel的下标
+        x = self.conv(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+class ConvBN(nn.Sequential):
+    def __init__(self, in_channel, out_channel, kernel_size=3, stride=1, groups=1):
+        # group如果等于输入特征模型的深度，则为DW卷积
+        padding = (kernel_size - 1) // 2
+        super(ConvBN, self).__init__(
+            nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_channel)
+        )
+
---
+++ STParNet/train_LSTM.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import json
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms, datasets
+from tqdm import tqdm
+# from par_LSTM import ParLSTM
+from par_DW_LSTM4 import ParDWLSTM
+
+
+def main():
+    batch_size = 32
+    num_classes = 5
+    epochs = 30
+    input_size = 9
+    hidden_size = 32
+    num_layers = 2
+
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    # transform = transforms.Compose([transforms.RandomResizedCrop(16),
+    #                                 transforms.RandomHorizontalFlip(),
+    #                                 transforms.ToTensor(),
+    #                                 transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "../"))  # get data root path
+    image_path = os.path.join(data_root, "LPN", "data_sequential_img")  # flower data set path
+    print(image_path)
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
+                                         transform=transform)
+    train_num = len(train_dataset)
+    print(train_num)
+
+    # # {'normal':0, 'DoS':1, 'Fuzzy':2, 'Gear':3, 'RPM':4}
+    # CAN_list = train_dataset.class_to_idx
+    # cla_dict = dict((val, key) for key, val in CAN_list.items())
+    # # write dict into json file
+    # json_str = json.dumps(cla_dict, indent=4)
+    #
+    # with open('class_indices.json', 'w') as json_file:
+    #     json_file.write(json_str)
+
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=nw)
+
+    validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                            transform=transform)
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=nw)
+
+    print("using {} images for training, {} images for validation.".format(train_num,
+                                                                           val_num))
+
+    # create model
+    net = ParDWLSTM(input_size=input_size, hidden_size=hidden_size,
+                  num_layers=num_layers, num_classes=num_classes, init_weights=True)
+
+    # # load pretrain weights （使用迁移学习，加载现成的权重）
+    # # download url: https://download.pytorch.org/models/mobilenet_v2-b0353104.pth
+    # model_weight_path = "./mobilenet_v2.pth"
+    # assert os.path.exists(model_weight_path), "file {} dose not exist.".format(model_weight_path)
+    # pre_weights = torch.load(model_weight_path, map_location='cpu')  # 载入后为字典类型
+    #
+    # # delete classifier weights
+    # pre_dict = {k: v for k, v in pre_weights.items() if net.state_dict()[k].numel() == v.numel()}
+    # # 遍历权重字典，看权重中是否含有对应参数，如果不在则直接保存到pre_dict字典当中
+    # missing_keys, unexpected_keys = net.load_state_dict(pre_dict, strict=False)  # 载入权重
+    #
+    # # freeze features weights （冻结特征提取的所有权重）
+    # for param in net.features.parameters():
+    #     param.requires_grad = False
+
+    # define loss function
+    loss_function = nn.CrossEntropyLoss()
+
+    # construct an optimizer
+    params = [p for p in net.parameters() if p.requires_grad]
+    optimizer = optim.Adam(params, lr=0.0001)
+
+    best_acc = 0.0
+
+    save_path = 'weight_res_seq/ParDWLSTM7_20_.pth'
+    train_steps = len(train_loader)
+    val_steps = len(validate_loader)
+    for epoch in range(epochs):
+        # train
+        net.train()
+        running_loss = 0.0
+        valing_loss = 0.0
+        train_acc = 0.0
+        train_bar = tqdm(train_loader, file=sys.stdout)
+        for step, data in enumerate(train_bar):
+            images, labels = data
+            optimizer.zero_grad()
+            logits = net(images)
+            predict_y = torch.max(logits, dim=1)[1]
+            train_acc += torch.eq(predict_y, labels).sum().item()
+            loss = loss_function(logits, labels)
+            loss.backward()
+            optimizer.step()
+            # print statistics
+            running_loss += loss.item()
+            train_bar.desc = "train epoch[{}/{}] loss:{:.4f}".format(epoch + 1,
+                                                                     epochs,
+                                                                     loss)
+
+
+        # validate
+        net.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        with torch.no_grad():
+            val_bar = tqdm(validate_loader, file=sys.stdout)
+            for val_data in val_bar:
+                val_images, val_labels = val_data
+                outputs = net(val_images)
+                val_loss = loss_function(outputs, val_labels)
+                # loss = loss_function(logits, labels)
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels).sum().item()
+                valing_loss += val_loss.item()
+
+                val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
+                                                           epochs)
+
+
+        train_accurate = train_acc / train_num
+        val_accurate = acc / val_num
+        print('[epoch %d] train_loss: %.4f  train_acc: %.4f  val_loss: %.4f  val_accuracy: %.4f' %
+              (epoch + 1, running_loss / train_steps, train_accurate, valing_loss / val_steps, val_accurate))
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
+
+    print('Finished Training')
+
+
+
+if __name__ == '__main__':
+    main()
